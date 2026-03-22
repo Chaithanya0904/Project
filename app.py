4287054d023c494d27d9eb9b9420cde2b411267d
Chaithanya0904 <141951867+Chaithanya0904@users.noreply.github.com>
2026-03-22 14:32:30 +0530

    Update app.py

--- app.py
+++ app.py
@@ -487,7 +487,7 @@ if __name__ == '__main__':
     socketio.run(
         app,
         host="0.0.0.0",
-        port=int(os.environ.get("PORT", 5000)),
+        port=int(os.environ.get("PORT", 8080)),
         debug=False,
         use_reloader=False,
         allow_unsafe_werkzeug=True
